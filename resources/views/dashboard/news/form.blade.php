7cd752b229e7fe7b9d1cf53bd7dde8e091b5aebb
jakasianturi <jakasianturi00@gmail.com>
2022-10-13 16:48:30 +0700

    summernote + lfm

--- resources/views/dashboard/news/form.blade.php
+++ resources/views/dashboard/news/form.blade.php
@@ -153,7 +153,7 @@
         $(document).ready(function() {
             // Define function to open filemanager window
             var lfm = function(options, cb) {
-                var route_prefix = (options && options.prefix) ? options.prefix : '/laravel-filemanager';
+                var route_prefix = '{{ url('/') }}/laravel-filemanager';
                 window.open(route_prefix + '?type=' + options.type || 'file', 'FileManager',
                     'width=900,height=600');
                 window.SetUrl = cb;
